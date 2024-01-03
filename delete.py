d4fe5c84b0a54fd42e7f03c92537e40df8d3706d
TRP <tirth4ur@gmail.com>
2024-01-03 13:27:22 -0500

    added custom file removal

--- delete.py
+++ delete.py
@@ -1,7 +1,30 @@
 import os
 import tkinter as tk
 
-path = "C:/Users/tirth/Downloads/xd.txt"
+path = "C:/Users/tirth/Downloads/"
+
+def deleteFile(preset_file=None):
+    
+    #destroys existing message
+    if hasattr(deleteFile, 'message'):
+      deleteFile.message.destroy()
+      
+    #checks if userinput file or preset
+    if preset_file is None:
+        file = entry1.get()
+    else:
+        file = preset_file
+    
+    file_path = path + file
+    
+    try: 
+        os.remove(file_path)
+        deleteFile.message = tk.Label(root, text=file + " removed", font=("Arial", 10, "bold"))
+    except FileNotFoundError:
+        deleteFile.message = tk.Label(root, text=file + " not found", font=("Arial", 10, "bold"))
+  
+    deleteFile.message.pack(padx=3, pady=3)
+
 
 #create window 
 root = tk.Tk()
@@ -9,22 +32,18 @@ root.geometry("500x500")
 root.title("File Remover")
 
 #title
-title = tk.Label(root, text="Remove File From Dowloads folder", font=("Ariel", 15, "bold"))
+title = tk.Label(root, text="Remove File From Dowloads", font=("Ariel", 15, "bold"))
 title.pack(padx=20, pady=20)
 
 #entry field
 entry1_label = tk.Label(root, text="Enter File Name", font=("Ariel", 12)) 
 entry1_label.pack(pady=5)
 
-entry1_label = tk.Entry(root) 
-entry1_label.pack(pady=0)
+entry1 = tk.Entry(root) 
+entry1.pack(pady=0)
 
-
-
+button1 = tk.Button(text='Delete', command=deleteFile)
+button1.pack(pady=10)
 root.mainloop()
 
-try: 
-    os.remove(path)
-except FileNotFoundError:
-    print("file not found")
     
